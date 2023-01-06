748d0ec02138dba6924fd2f18701970ea25d6566
Anna Nosyk <anna.nosyk89@gmail.com>
2023-01-06 17:14:56 +0100

    - add app icon  - fix dark and light mode

--- MyMysic/Controllers/MyMusicVC.swift
+++ MyMysic/Controllers/MyMusicVC.swift
@@ -19,7 +19,7 @@ class MyMusicVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+       // view.backgroundColor = .white
        
     }
 
